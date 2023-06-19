740982b2cfb56bc95affa60e71ca2005a947ccfa
Peter Ademiju <duyox2004@gmail.com>
2023-06-19 20:41:48 +0100

    initial commit

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\DOLAPO\DEVELOPER\Violet Python Class\env
+set VIRTUAL_ENV=C:\Users\HP\Documents\Capstone Backend\API-CUT\env
 
 if not defined PROMPT set PROMPT=$P$G
 
